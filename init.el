07c559518cc1d7cff2540c6675e57018d7385b94
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2017-01-30 17:14:22 +0200

    stuff

--- init.el
+++ init.el
@@ -4,7 +4,6 @@
    dotspacemacs-configuration-layer-path '()
    dotspacemacs-configuration-layers
    '(html
-     auto-completion
      (dash :variables
            dash-helm-dash-docset-path "~/.docsets")
      (auto-completion :variables
@@ -26,7 +25,7 @@
      ranger
      rust
      scheme
-     spacemacs-layouts
+     ;; spacemacs-layouts
      (shell :variables
             shell-default-height 20
             shell-default-position 'bottom
@@ -34,7 +33,7 @@
             shell-protect-eshell-prompt t)
      ;; spell-checking
      version-control)
-   dotspacemacs-additional-packages '(key-chord scss-mode uuidgen simpleclip darktooth-theme lispy lispyville)
+   dotspacemacs-additional-packages '(key-chord scss-mode uuidgen simpleclip lispy lispyville)
    dotspacemacs-excluded-packages '(evil-escape linum multiple-cursors)
    dotspacemacs-delete-orphan-packages t))
 
@@ -49,7 +48,7 @@
    dotspacemacs-startup-lists '(recents projects)
    dotspacemacs-startup-recent-list-size 5
    dotspacemacs-scratch-mode 'emacs-lisp-mode
-   dotspacemacs-themes '(darktooth
+   dotspacemacs-themes '(
                          spacemacs-dark
                          solarized-dark
                          spacemacs-light
@@ -95,7 +94,9 @@
    dotspacemacs-persistent-server nil
    dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
    dotspacemacs-default-package-repository nil
-   dotspacemacs-whitespace-cleanup 'all))
+   dotspacemacs-whitespace-cleanup 'all
+   dotspacemacs-helm-use-fuzzy 'source
+   ))
 
 (defun dotspacemacs/user-init ()
   (add-to-load-path "~/.spacemacs.d/packages"))
@@ -178,12 +179,20 @@
   (bind-key* "M-7" 'select-window-7)
   (bind-key* "M-8" 'select-window-8)
   (bind-key* "M-9" 'select-window-9)
+  (bind-key* "M-J" 'evil-mc-make-cursor-move-next-line)
+  (bind-key* "M-K" 'evil-mc-make-cursor-move-prev-line)
 
   (spaceline-toggle-minor-modes-off)
+  (global-evil-mc-mode)
 
   (load-my-packages)
 
   (define-key evil-normal-state-map (kbd "<SPC>qq") 'undefined)
+  (define-key evil-normal-state-map (kbd "gg") 'evil-mc-undo-all-cursors)
+  (define-key evil-visual-state-map (kbd "gg") 'evil-mc-undo-all-cursors)
+  (define-key evil-normal-state-map (kbd "C-<backspace>") 'undefined)
+  (define-key evil-insert-state-map (kbd "C-<backspace>") 'undefined)
+  (define-key evil-visual-state-map (kbd "C-<backspace>") 'undefined)
   (evil-define-key 'normal helm-map (kbd "<RET>") 'my/helm-exit-minibuffer)
   (key-chord-define-global "xs" 'evil-normal-state-and-save)
 
@@ -196,7 +205,13 @@
   (add-hook* 'rust-mode-hook (setq-local helm-dash-docsets '("Rust")))
   (add-hook* 'prog-mode-hook (key-chord-mode 1))
   (add-hook* 'isearch-mode-hook (key-chord-mode 1))
-  (add-hook* 'before-save-hook (indent-buffer-on-save)))
+  (add-hook* 'before-save-hook (indent-buffer-on-save))
+
+  (spacemacs|define-custom-layout "nairte"
+    :binding "n")
+
+  (spacemacs|define-custom-layout "client"
+    :binding "c"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -205,10 +220,7 @@
  ;; If there is more than one, they won't work right.
  '(anzu-cons-mode-line-p nil)
  '(anzu-mode-line-update-function (quote spacemacs/anzu-update-mode-line))
- '(auto-compile-mode-line-counter t)
  '(elm-format-on-save t)
- '(elscreen-tab-display-control nil)
- '(elscreen-tab-display-kill-screen nil)
  '(erc-image-inline-rescale 1)
  '(erc-prompt-for-password nil)
  '(evil-want-Y-yank-to-eol t)
@@ -219,7 +231,7 @@
  '(helm-make-comint t)
  '(package-selected-packages
    (quote
-    (ruby-end zeal-at-point yaml-mode xterm-color ws-butler window-numbering which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package toml-mode toc-org tagedit spacemacs-theme spaceline smeargle slim-mode simpleclip shell-pop scss-mode sass-mode restart-emacs ranger rainbow-delimiters racer quelpa pug-mode popwin persp-mode pcre2el paradox orgit org-projectile org-present org-pomodoro org-plus-contrib org-download org-bullets open-junk-file ob-elixir neotree multi-term move-text mmm-mode markdown-toc magit-gitflow macrostep lua-mode lorem-ipsum livid-mode lispyville linum-relative link-hint less-css-mode key-chord js2-refactor js-doc info+ indent-guide ido-vertical-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-dash helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md geiser flycheck-rust flycheck-pos-tip flycheck-mix flycheck-elm flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-ediff evil-args evil-anzu eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emmet-mode elm-mode elisp-slime-nav dumb-jump dockerfile-mode docker diff-hl define-word darktooth-theme company-web company-tern company-statistics company-quickhelp column-enforce-mode coffee-mode clojure-snippets clj-refactor clean-aindent-mode cider-eval-sexp-fu cargo auto-yasnippet auto-highlight-symbol auto-compile alchemist aggressive-indent adaptive-wrap ace-link ace-jump-helm-line ac-ispell)))
+    (powerline parent-mode flx iedit anzu evil goto-chg undo-tree f diminish s spinner bind-map bind-key packed dash pkg-info epl avy async popup package-build smartparens highlight helm helm-core projectile hydra zeal-at-point yaml-mode xterm-color web-mode web-beautify toml-mode tagedit smeargle slim-mode simpleclip shell-pop scss-mode sass-mode ranger racer pug-mode orgit org-projectile org-present org-pomodoro alert log4e gntp org-download ob-elixir org multi-term mmm-mode markdown-toc markdown-mode magit-gitflow lua-mode livid-mode skewer-mode simple-httpd lispyville lispy zoutline swiper ivy less-css-mode key-chord js2-refactor js2-mode js-doc htmlize helm-gitignore helm-dash helm-css-scss helm-company helm-c-yasnippet haml-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md geiser flycheck-rust flycheck-pos-tip flycheck-mix flycheck-elm flycheck evil-magit magit git-commit with-editor eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emmet-mode elm-mode dockerfile-mode docker json-mode tablist magit-popup docker-tramp json-snatcher json-reformat diff-hl company-web web-completion-data company-tern dash-functional tern company-statistics company-quickhelp pos-tip coffee-mode clojure-snippets clj-refactor inflections edn multiple-cursors paredit peg cider-eval-sexp-fu cider seq queue clojure-mode cargo rust-mode auto-yasnippet yasnippet alchemist company elixir-mode ac-ispell auto-complete ws-butler window-numbering which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spacemacs-theme spaceline restart-emacs request rainbow-delimiters quelpa popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide ido-vertical-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line)))
  '(paradox-github-token t)
  '(projectile-enable-caching t)
  '(spaceline-helm-mode t)
--- packages/package-multiple-cursors.el
+++ packages/package-multiple-cursors.el
@@ -1,14 +1,14 @@
-(use-package multiple-cursors
-  :load-path "~/.spacemacs.d/multiple-cursors.el/"
-  :bind* (("M-K" . mc/mark-previous-like-this)
-          ("M-J" . mc/mark-next-like-this))
-  :bind (:map mc/keymap
-              ("<return>" . newline))
-  :init
-  (ensure-clone "myrjola" "multiple-cursors.el" "evil-compat")
-  :config
-  (multiple-cursors-mode t)
-  (define-key org-mode-map (kbd "M-N") 'mc/mark-next-like-this)
-  (define-key org-mode-map (kbd "M-P") 'mc/mark-previous-like-this))
+;; (use-package multiple-cursors
+;;   :load-path "~/.spacemacs.d/multiple-cursors.el/"
+;;   :bind* (("M-K" . mc/mark-previous-like-this)
+;;           ("M-J" . mc/mark-next-like-this))
+;;   :bind (:map mc/keymap
+;;               ("<return>" . newline))
+;;   :init
+;;   (ensure-clone "myrjola" "multiple-cursors.el" "evil-compat")
+;;   :config
+;;   (multiple-cursors-mode t)
+;;   (define-key org-mode-map (kbd "M-N") 'mc/mark-next-like-this)
+;;   (define-key org-mode-map (kbd "M-P") 'mc/mark-previous-like-this))
 
 (provide 'package-multiple-cursors)
