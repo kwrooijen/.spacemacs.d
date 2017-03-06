55370120e2960c026b1c978ccd6408454247319d
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2017-03-06 15:29:01 +0200

    WIP

---
+++ functions.el
@@ -0,0 +1,45 @@
+(defmacro add-hook* (mode fn)
+  `(add-hook ,mode (lambda () ,fn)))
+
+(defmacro ensure-clone (user project branch)
+  (unless (file-exists-p (format "~/.spacemacs.d/%s" project))
+    (shell-command (format "cd ~/.spacemacs.d && git clone -b %s git@github.com:%s/%s" branch user project))))
+
+(defun capitalize-previous-word ()
+  (interactive)
+  (save-excursion
+    (backward-word)
+    (capitalize-word 1)))
+
+(defun eww-other-window (url)
+  (interactive)
+  (view-buffer-other-window "*DocBuffer*")
+  (eww url))
+
+(defun evil-normal-state-and-save ()
+  (interactive)
+  (evil-normal-state)
+  (save-buffer))
+
+(defun indent-buffer-on-save ()
+  (if (member major-mode indent-buffer-modes)
+      (spacemacs/indent-region-or-buffer)))
+
+(defun my/helm-exit-minibuffer ()
+  (interactive)
+  (helm-exit-minibuffer))
+
+(defun require-template (symbol)
+  `(require (quote ,symbol)))
+
+(defmacro load-my-packages ()
+  (let* ((filter-fn (lambda (x) (not (member x '("." "..")))))
+         (strip-ext-fn (lambda (x) (s-left -3 x)))
+         (all-files (directory-files "~/.spacemacs.d/packages/"))
+         (package-files-ext (-filter filter-fn all-files))
+         (package-files (-map strip-ext-fn package-files-ext))
+         (package-symbols (-map 'read package-files))
+         (package-requires (-map 'require-template package-symbols)))
+    `(progn ,@package-requires)))
+
+(provide 'functions)
--- init.el
+++ init.el
@@ -4,8 +4,7 @@
    dotspacemacs-configuration-layer-path '()
    dotspacemacs-configuration-layers
    '(html
-     (dash :variables
-           dash-helm-dash-docset-path "~/.docsets")
+     dash
      (auto-completion :variables
                       auto-completion-enable-help-tooltip t)
      elixir
@@ -33,7 +32,7 @@
             shell-protect-eshell-prompt t)
      ;; spell-checking
      version-control)
-   dotspacemacs-additional-packages '(key-chord scss-mode uuidgen simpleclip lispy lispyville)
+   dotspacemacs-additional-packages '(key-chord scss-mode uuidgen simpleclip lispy lispyville rainbow-mode)
    dotspacemacs-excluded-packages '(evil-escape linum multiple-cursors)
    dotspacemacs-delete-orphan-packages t))
 
@@ -48,14 +47,7 @@
    dotspacemacs-startup-lists '(recents projects)
    dotspacemacs-startup-recent-list-size 5
    dotspacemacs-scratch-mode 'emacs-lisp-mode
-   dotspacemacs-themes '(
-                         spacemacs-dark
-                         solarized-dark
-                         spacemacs-light
-                         solarized-light
-                         leuven
-                         monokai
-                         zenburn)
+   dotspacemacs-themes '(spacemacs-dark spacemacs-light)
    dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-default-font '("Fira Mono"
                                :size 13
@@ -99,150 +91,60 @@
    ))
 
 (defun dotspacemacs/user-init ()
-  (add-to-load-path "~/.spacemacs.d/packages"))
+  (add-to-load-path "~/.spacemacs.d/packages")
+  (add-to-load-path "~/.spacemacs.d"))
 
 (defun dotspacemacs/user-config ()
-  (defmacro add-hook* (mode fn)
-    `(add-hook ,mode (lambda () ,fn)))
-
-  (defmacro ensure-clone (user project branch)
-    (unless (file-exists-p (format "~/.spacemacs.d/%s" project))
-      (shell-command (format "cd ~/.spacemacs.d && git clone -b %s git@github.com:%s/%s" branch user project))))
-
-  (defun capitalize-previous-word ()
-    (interactive)
-    (save-excursion
-      (backward-word)
-      (capitalize-word 1)))
-
-  (defun eww-other-window (url)
-    (interactive)
-    (view-buffer-other-window "*DocBuffer*")
-    (eww url))
-
-  (defun evil-normal-state-and-save ()
-    (interactive)
-    (evil-normal-state)
-    (save-buffer))
-
-  (defun indent-buffer-on-save ()
-    (if (member major-mode indent-buffer-modes)
-        (spacemacs/indent-region-or-buffer)))
-
-  (defun my/helm-exit-minibuffer ()
-    (interactive)
-    (helm-exit-minibuffer))
-
-  (defun require-template (symbol)
-    `(require (quote ,symbol)))
-
-  (defmacro load-my-packages ()
-    (let* ((filter-fn (lambda (x) (not (member x '("." "..")))))
-           (strip-ext-fn (lambda (x) (s-left -3 x)))
-           (all-files (directory-files "~/.spacemacs.d/packages/"))
-           (package-files-ext (-filter filter-fn all-files))
-           (package-files (-map strip-ext-fn package-files-ext))
-           (package-symbols (-map 'read package-files))
-           (package-requires (-map 'require-template package-symbols)))
-      `(progn ,@package-requires)))
-
-  (setq diff-hl-side 'left
-        git-gutter-fr+-side 'left-fringe
-        alchemist-test--mode-name-face nil
-        x-select-enable-clipboard nil
-        indent-buffer-modes '(scss-mode)
-        projectile-use-git-grep t
-        scroll-error-top-bottom t
-        helm-dash-browser-func 'eww-other-window
-        helm-dash-docsets-path "~/.docsets"
-        helm-make-named-buffer t)
-
-  (setq neo-smart-open nil
-        neo-dont-be-alone nil
-        neo-persist-show nil
-        neo-show-hidden-files nil
-        neo-auto-indent-point nil
-        neo-modern-sidebar nil)
-
-  (bind-key* "C-S-V" 'simpleclip-paste)
-  (bind-key* "C-S-C" 'simpleclip-copy)
-  (bind-key* "C-S-X" 'simpleclip-cut)
-  (bind-key* "M-u" 'undo-tree-redo)
-  (bind-key* "M-+" 'align-regexp)
-  (bind-key* "M-C" 'capitalize-previous-word)
-  (bind-key* "M-1" 'select-window-1)
-  (bind-key* "M-2" 'select-window-2)
-  (bind-key* "M-3" 'select-window-3)
-  (bind-key* "M-4" 'select-window-4)
-  (bind-key* "M-5" 'select-window-5)
-  (bind-key* "M-6" 'select-window-6)
-  (bind-key* "M-7" 'select-window-7)
-  (bind-key* "M-8" 'select-window-8)
-  (bind-key* "M-9" 'select-window-9)
-  (bind-key* "M-J" 'evil-mc-make-cursor-move-next-line)
-  (bind-key* "M-K" 'evil-mc-make-cursor-move-prev-line)
+  (require 'functions)
+  (require 'keys)
 
   (spaceline-toggle-minor-modes-off)
   (global-evil-mc-mode)
-
   (load-my-packages)
 
-  (define-key evil-normal-state-map (kbd "<SPC>qq") 'undefined)
-  (define-key evil-normal-state-map (kbd "gg") 'evil-mc-undo-all-cursors)
-  (define-key evil-visual-state-map (kbd "gg") 'evil-mc-undo-all-cursors)
-  (define-key evil-normal-state-map (kbd "C-<backspace>") 'undefined)
-  (define-key evil-insert-state-map (kbd "C-<backspace>") 'undefined)
-  (define-key evil-visual-state-map (kbd "C-<backspace>") 'undefined)
-  (evil-define-key 'normal helm-map (kbd "<RET>") 'my/helm-exit-minibuffer)
-  (key-chord-define-global "xs" 'evil-normal-state-and-save)
-
-  (add-hook* 'clojure-mode-hook (setq-local helm-dash-docsets '("Clojure")))
-  (add-hook* 'elixir-mode-hook (setq-local helm-dash-docsets '("Elixir")))
-  (add-hook* 'emacs-lisp-mode-hook (setq-local helm-dash-docsets '("Emacs Lisp")))
-  (add-hook* 'erlang-mode-hook (setq-local helm-dash-docsets '("Erlang")))
-  (add-hook* 'haskell-mode-hook (setq-local helm-dash-docsets '("Haskell")))
-  (add-hook* 'ruby-mode-hook (setq-local helm-dash-docsets '("Ruby")))
-  (add-hook* 'rust-mode-hook (setq-local helm-dash-docsets '("Rust")))
   (add-hook* 'prog-mode-hook (key-chord-mode 1))
   (add-hook* 'isearch-mode-hook (key-chord-mode 1))
-  (add-hook* 'before-save-hook (indent-buffer-on-save))
 
-  (spacemacs|define-custom-layout "nairte"
-    :binding "n")
-
-  (spacemacs|define-custom-layout "client"
-    :binding "c"))
-
+  (setq
+   alchemist-test--mode-name-face nil
+   anzu-cons-mode-line-p nil
+   anzu-mode-line-update-function 'spacemacs/anzu-update-mode-line
+   diff-hl-side 'left
+   elm-format-on-save t
+   flycheck-pos-tip-timeout 9999
+   git-gutter-fr+-side 'left-fringe
+   global-hl-line-mode t
+   global-page-break-lines-mode t
+   helm-dash-browser-func 'eww-other-window
+   helm-display-header-line nil
+   helm-echo-input-in-header-line t
+   helm-make-comint t
+   helm-make-named-buffer t
+   indent-buffer-modes '(scss-mode)
+   neo-auto-indent-point nil
+   neo-dont-be-alone nil
+   neo-modern-sidebar nil
+   neo-persist-show nil
+   neo-show-hidden-files nil
+   neo-smart-open nil
+   projectile-enable-caching t
+   projectile-use-git-grep t
+   scroll-error-top-bottom t
+   spaceline-helm-mode t
+   spaceline-info-mode t
+   x-select-enable-clipboard nil
+   ))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(anzu-cons-mode-line-p nil)
- '(anzu-mode-line-update-function (quote spacemacs/anzu-update-mode-line))
- '(elm-format-on-save t)
- '(erc-image-inline-rescale 1)
- '(erc-prompt-for-password nil)
- '(evil-want-Y-yank-to-eol t)
- '(global-hl-line-mode t)
- '(global-page-break-lines-mode t)
- '(helm-display-header-line nil)
- '(helm-echo-input-in-header-line t)
- '(helm-make-comint t)
  '(package-selected-packages
    (quote
-    (powerline parent-mode flx iedit anzu evil goto-chg undo-tree f diminish s spinner bind-map bind-key packed dash pkg-info epl avy async popup package-build smartparens highlight helm helm-core projectile hydra zeal-at-point yaml-mode xterm-color web-mode web-beautify toml-mode tagedit smeargle slim-mode simpleclip shell-pop scss-mode sass-mode ranger racer pug-mode orgit org-projectile org-present org-pomodoro alert log4e gntp org-download ob-elixir org multi-term mmm-mode markdown-toc markdown-mode magit-gitflow lua-mode livid-mode skewer-mode simple-httpd lispyville lispy zoutline swiper ivy less-css-mode key-chord js2-refactor js2-mode js-doc htmlize helm-gitignore helm-dash helm-css-scss helm-company helm-c-yasnippet haml-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md geiser flycheck-rust flycheck-pos-tip flycheck-mix flycheck-elm flycheck evil-magit magit git-commit with-editor eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emmet-mode elm-mode dockerfile-mode docker json-mode tablist magit-popup docker-tramp json-snatcher json-reformat diff-hl company-web web-completion-data company-tern dash-functional tern company-statistics company-quickhelp pos-tip coffee-mode clojure-snippets clj-refactor inflections edn multiple-cursors paredit peg cider-eval-sexp-fu cider seq queue clojure-mode cargo rust-mode auto-yasnippet yasnippet alchemist company elixir-mode ac-ispell auto-complete ws-butler window-numbering which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spacemacs-theme spaceline restart-emacs request rainbow-delimiters quelpa popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide ido-vertical-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line)))
- '(paradox-github-token t)
- '(projectile-enable-caching t)
- '(spaceline-helm-mode t)
- '(spaceline-info-mode t))
+    (rainbow-mode zeal-at-point yaml-mode xterm-color ws-butler window-numbering which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package toml-mode toc-org tagedit spacemacs-theme spaceline smeargle slim-mode simpleclip shell-pop scss-mode sass-mode restart-emacs ranger rainbow-delimiters racer quelpa pug-mode popwin persp-mode pcre2el paradox orgit org-projectile org-present org-pomodoro org-plus-contrib org-download org-bullets open-junk-file ob-elixir neotree multi-term move-text mmm-mode markdown-toc magit-gitflow macrostep lua-mode lorem-ipsum livid-mode lispyville linum-relative link-hint less-css-mode key-chord js2-refactor js-doc info+ indent-guide ido-vertical-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-dash helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md geiser flycheck-rust flycheck-pos-tip flycheck-mix flycheck-elm flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-ediff evil-args evil-anzu eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emmet-mode elm-mode elisp-slime-nav dumb-jump dockerfile-mode docker diff-hl define-word company-web company-tern company-statistics company-quickhelp column-enforce-mode coffee-mode clojure-snippets clj-refactor clean-aindent-mode cider-eval-sexp-fu cargo auto-yasnippet auto-highlight-symbol auto-compile alchemist aggressive-indent adaptive-wrap ace-link ace-jump-helm-line ac-ispell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(alchemist-test--failed-face ((t (:inherit font-lock-variable-name-face :foreground "tomato" :weight bold))))
- '(alchemist-test--success-face ((t (:inherit font-lock-variable-name-face :foreground "green" :weight bold))))
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
- '(header-line ((t (:inherit (quote default) :background nil)))))
+ )
