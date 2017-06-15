f71a43cc83c83e7e9dee45d5a5e3ac40321f51c2
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2017-06-15 15:32:11 +0300

    WIP

--- init.el
+++ init.el
@@ -145,6 +145,16 @@
   (add-hook 'emacs-lisp-mode-hook #'turn-off-smartparens-mode)
   (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
 
+  (add-hook 'yas-before-expand-snippet-hook
+            (lambda ()
+              (when (member major-mode '(clojure-mode emacs-lisp-mode))
+                (parinfer-mode -1))))
+
+  (add-hook 'yas-after-exit-snippet-hook
+            (lambda ()
+              (when (member major-mode '(clojure-mode emacs-lisp-mode))
+                (parinfer-mode))))
+
   (defun parinfer-lispy:backward-insert ()
     (interactive)
     (parinfer-lispy:backward)
@@ -160,7 +170,7 @@
           (parinfer--invoke-parinfer-instantly)
           (back-to-indentation))))
 
-  (defadvice parinfer-smart-tab:dwim-right (before parinfer-smart-tab:dwim-right activate)
+  (defadvice parinfer-smart-tab:dwim-right (before parinfer-smart-tab:dwim-right-before activate)
     (if (region-active-p)
         (progn
           (evil-visual-char)
@@ -168,15 +178,42 @@
           (backward-char 1)
           (beginning-of-line))))
 
+  (defadvice parinfer-smart-tab:dwim-left (before parinfer-smart-tab:dwim-left-before activate)
+    (if (region-active-p)
+        (progn
+          (evil-visual-char)
+          (if (< (point) (region-end)) (exchange-point-and-mark))
+          (backward-char 1)
+          (beginning-of-line))))
+
+  (defadvice parinfer-smart-tab:dwim-right (after parinfer-smart-tab:dwim-right-after activate)
+    (back-to-indentation))
+
+  (defadvice parinfer-smart-tab:dwim-left (after parinfer-smart-tab:dwim-left-after activate)
+    (back-to-indentation))
+
+  (defun my-parinfer-toggle-mode ()
+    "docstring"
+    (interactive)
+    (if parinfer-mode
+        (parinfer-toggle-mode)
+      (parinfer-mode)))
+
   (defun my-parinfer-mode-hook ()
+    (define-key parinfer-mode-map (kbd "M-]") 'parinfer--reindent-sexp)
     (define-key parinfer-mode-map (kbd "M-m") 'back-to-indentation)
     (define-key parinfer-mode-map (kbd "M-a") 'parinfer-lispy:backward-insert)
-    (define-key parinfer-mode-map (kbd "M-q") 'parinfer-toggle-mode)
     (define-key parinfer-mode-map (kbd "M-{") 'paredit-backward-slurp-sexp)
     (define-key parinfer-mode-map (kbd "M-}") 'paredit-forward-slurp-sexp)
     (define-key parinfer-mode-map (kbd "M-j") 'paredit-join-sexps)
+    (define-key parinfer-mode-map (kbd "<backtab>") 'parinfer-smart-tab:dwim-left)
     (define-key parinfer-mode-map (kbd "<tab>") 'parinfer-smart-tab:dwim-right)
+    (define-key parinfer-region-mode-map (kbd "<backtab>") 'parinfer-smart-tab:dwim-left)
     (define-key parinfer-region-mode-map (kbd "<tab>") 'parinfer-smart-tab:dwim-right))
+
+  (require 'clojure-mode)
+  (define-key clojure-mode-map (kbd "M-q") 'my-parinfer-toggle-mode)
+  (define-key emacs-lisp-mode-map (kbd "M-q") 'my-parinfer-toggle-mode)
 
   (add-hook 'parinfer-mode-hook 'my-parinfer-mode-hook)
 
