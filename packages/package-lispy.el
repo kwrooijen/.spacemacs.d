f50c8d668768ca71ec5c6be2b5f32159c13faec4
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2017-08-05 19:59:36 +0300

    Added lispy tweaks

--- packages/package-lispy.el
+++ packages/package-lispy.el
@@ -1,20 +1,32 @@
-;; Lispy
 (require 'lispyville)
 (require 'lispy)
+
 (add-hook 'lispy-mode-hook #'lispyville-mode)
 (add-hook 'emacs-lisp-mode-hook #'lispy-mode)
 (add-hook 'clojure-mode-hook #'lispy-mode)
 
-(evil-define-key 'normal lispy-mode-map
-  "x" #'lispy-delete)
+(evil-define-key 'insert lispy-mode-map
+  "[" #'lispy-brackets-or-barf)
 
 (evil-define-key 'insert lispy-mode-map
-  "[" #'lispy-brackets)
+  (kbd "M-[") #'lispy-wrap-brackets-or-reverse-barf)
+
+(defun lispy--mode-p ()
+  (or (lispy-left-p)
+      (lispy-right-p)))
 
 (defun lispy-left-insert ()
   (interactive)
-  (when (not (eql 40 (char-after)))
+  (when (not (lispy--mode-p))
     (lispy-left 1))
+  (when (not (lispy--mode-p))
+    (beginning-of-defun))
+  (evil-insert-state 1))
+
+(defun lispy-beginning-of-defun-insert ()
+  (interactive)
+  (end-of-line)
+  (beginning-of-defun)
   (evil-insert-state 1))
 
 (defun lispy-global-teleport (arg)
@@ -22,14 +34,37 @@
   (let ((lispy-teleport-global t))
     (lispy-teleport arg)))
 
+(defun lispy-brackets-or-barf (arg)
+  (interactive "P")
+  (if (lispy--mode-p)
+      (lispy-barf 1)
+    (lispy-brackets arg)))
+
+(defun lispy-wrap-brackets-or-reverse-barf ()
+  (interactive)
+  (if (lispy--mode-p)
+      (save-excursion
+        (lispy-different)
+        (lispy-barf 1))
+    (lispy-wrap-brackets)))
+
+(defun lispy-reverse-slurp ()
+  (interactive)
+  (save-excursion
+    (lispy-different)
+    (lispy-slurp 1)))
+
 (eval-after-load "lispy"
   `(progn
      (define-key lispyville-mode-map (kbd "M-w") 'lispyville-yank)
      (define-key lispy-mode-map (kbd "M-a") 'lispy-left-insert)
+     (define-key lispy-mode-map (kbd "M-A") 'lispy-beginning-of-defun-insert)
      (define-key lispy-mode-map (kbd "C-S") 'lispy-unstringify)
      (define-key lispy-mode-map (kbd "M-i") 'tab-to-tab-stop)
-     (lispy-define-key lispy-mode-map "]" 'special-lispy-slurp)
-     (lispy-define-key lispy-mode-map "[" 'special-lispy-barf)
+     (lispy-define-key lispy-mode-map "M-]" 'lispy-reverse-slurp)
+     (lispy-define-key lispy-mode-map "o" 'evil-open-below)
+     (lispy-define-key lispy-mode-map "x" 'lispy-delete)
+     (lispy-define-key lispy-mode-map "]" 'lispy-slurp)
      (lispy-define-key lispy-mode-map "A" 'lispy-ace-symbol-replace)
      (lispy-define-key lispy-mode-map "H" 'special-lispy-move-left)
      (lispy-define-key lispy-mode-map "J" 'special-lispy-down-slurp)
