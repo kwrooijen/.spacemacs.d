d873324054cee7ce954ecc15b9fe90e8db20b364
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2019-12-11 15:09:48 +0100

    Add Clojure configuration

--- packages/package-clojure.el
+++ packages/package-clojure.el
@@ -1,5 +1,7 @@
 (require 'functions)
 (require 'cider)
+(require 'flycheck-joker)
+(require 'flycheck-clj-kondo)
 
 (setq cider-auto-jump-to-error nil
       ;; cider-lein-parameters "trampoline repl :headless :host ::"
@@ -51,5 +53,20 @@
   (s/fdef 1)
   (dom/div 1)
   (let-if 1))
+
+(dolist (checker '(clj-kondo-clj clj-kondo-cljs clj-kondo-cljc clj-kondo-edn))
+ (setq flycheck-checkers (cons checker (delq checker flycheck-checkers))))
+(dolist (checkers '((clj-kondo-clj . clojure-joker)
+                   (clj-kondo-cljs . clojurescript-joker)
+                   (clj-kondo-cljc . clojure-joker)
+                   (clj-kondo-edn . edn-joker)))
+ (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers))))
+
+(add-hook 'clojure-mode-hook 'flycheck-mode)
+
+(use-package clojure-mode
+ :ensure t
+ :config
+ (require 'flycheck-clj-kondo))
 
 (provide 'package-clojure)
